73297d9d38b19d8852475a54a422a853da55c6db
PowerxNetwork <52278911+PowerxNetwork@users.noreply.github.com>
2019-07-03 18:30:19 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-   client.user.setActivity("✧ Bots Music",{type: 'WATCHING'})
+   client.user.setActivity("✧ Music  Bots",{type: 'WATCHING'})
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
